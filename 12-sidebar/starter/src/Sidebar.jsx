c23fa01449216f635076b00903a0acb1f9c63a0f
Xmorfin <bimosullivan10@gmail.com>
2023-09-10 13:37:37 +0700

    sidebar component

--- 12-sidebar/starter/src/Sidebar.jsx
+++ 12-sidebar/starter/src/Sidebar.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { useGlobalContext } from "./Context";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { links, social } from "./data";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className={isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}>
+      <div className="sidebar-header">
+        <img src={logo} alt="logo" />
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+
+      {/* lINKS MAP */}
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+
+      {/* SOCIAL LINKS MAP */}
+      <ul className="social-links">
+        {social.map((link) => {
+          const { id, url, icon } = link;
+
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 };
 
 export default Sidebar;
